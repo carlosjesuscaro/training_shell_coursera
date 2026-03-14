16dbafefeb48ed94469e23ba87568115f83cbb84
Carlos Jesus Caro <carlos.jesus.caro@gmail.com>
2026-03-14 14:20:22 -0400

    feat: Adding the TODO for the access_data script

---
+++ scripts/access_data.bash
@@ -0,0 +1,8 @@
+# TODO: 1. Collect a domain from the user
+# TODO: 2. Validate the site is up (200 response)
+# TODO: 3. Download the HTML file
+# TODO: 4. Ask the user for keywords
+# TODO: 5. Present how many times the key word is in the file
+
+
+
